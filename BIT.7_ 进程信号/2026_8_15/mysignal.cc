efad1a607ebcbc72d8ca06024b8111ae4fd360bc
Li2ladybro <3258347687@qq.com>
2026-08-16 22:27:35 +0800

    信号处理

--- BIT.7_ 进程信号/2026_8_15/mysignal.cc
+++ BIT.7_ 进程信号/2026_8_15/mysignal.cc
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <vector>
+
 #include <unistd.h>
 #include <signal.h>
 
 using namespace std;
 
-#define BLAOCK_SIGANL 2
+// #define BLAOCK_SIGANL 2
 #define MAX_SIIGNUM 31
+
+int arr[2] = {2, 3};
 
 void showpending(const sigset_t &pending)
 {
@@ -13,7 +17,7 @@ void showpending(const sigset_t &pending)
     // {
     //     cout << e << endl;
     // }
-    for (int i = 1; i < MAX_SIIGNUM; ++i)
+    for (int i = MAX_SIIGNUM; i > 0; --i)
     {
         if (sigismember(&pending, i))
         {
@@ -27,6 +31,11 @@ void showpending(const sigset_t &pending)
     cout << endl;
 }
 
+void myHander(int sigNum)
+{
+    cout << "我捕捉到了" << sigNum << "信号信号并递达\n";
+}
+
 int main()
 {
     sigset_t block, oblock, pending;
@@ -35,21 +44,32 @@ int main()
     sigemptyset(&oblock);
 
     // 1.2、添加需要屏蔽的信号
-    sigaddset(&block, BLAOCK_SIGANL);
+    for (auto &e : arr)
+    {
+        sigaddset(&block, e);
+        signal(e, myHander);
+    }
 
     // 1.3、开始屏蔽信号
     sigprocmask(SIG_SETMASK, &block, &oblock);
 
+    int cun = 10;
     // 2、遍历打印pending
     while (true)
     {
         // 2.1、初始化
         sigemptyset(&pending);
         // 2.2、获取
+        // 内核把内核态的未决信号集复制到你的 pending 变量，直接覆盖。
         sigpending(&pending);
         // 2.3、打印
         showpending(pending);
         sleep(1);
+        if (cun-- == 0)
+        {
+            cout << "恢复对信号的屏蔽，不去屏蔽任何信号\n";
+            sigprocmask(SIG_SETMASK, &oblock, &block);
+        }
     }
 
     return 0;

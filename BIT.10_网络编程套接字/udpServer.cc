50de8794ec2cd903a768720202ee5ae7293c4a4a
Li2ladybro <3258347687@qq.com>
2026-09-15 16:05:42 +0800

    Socket初

---
+++ BIT.10_网络编程套接字/udpServer.cc
@@ -0,0 +1,12 @@
+#include "udpServer.hpp"
+#include <memory>
+
+
+
+int main()
+{
+    std::unique_ptr<udpServer> usvr(new udpServer()); 
+    usvr->initServer();
+    usvr->start();
+    return 0;
+}

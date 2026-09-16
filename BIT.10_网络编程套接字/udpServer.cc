d453fff89537d57398e556866afc3a3f5810fe86
Li2ladybro <3258347687@qq.com>
2026-09-16 20:38:45 +0800

    套接字

--- BIT.10_网络编程套接字/udpServer.cc
+++ BIT.10_网络编程套接字/udpServer.cc
@@ -1,11 +1,27 @@
 #include "udpServer.hpp"
 #include <memory>
 
+using namespace Server;
 
+using namespace std;
 
-int main()
+static void Usage(string proc)
 {
-    std::unique_ptr<udpServer> usvr(new udpServer()); 
+    cout << "\nUsage:\n\t" << proc << " loacl_ip Iocal_port\n\n";
+}
+
+// ./udpServer ip port
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        Usage(argv[0]);
+        exit(-1);
+    }
+    uint16_t port = atoi(argv[2]);
+    string ip = argv[1];
+
+    std::unique_ptr<udpServer> usvr(new udpServer(port, ip));
     usvr->initServer();
     usvr->start();
     return 0;
--- BIT.10_网络编程套接字/udpServer.hpp
+++ BIT.10_网络编程套接字/udpServer.hpp
@@ -1,24 +1,84 @@
 #pragma once
 
-class udpServer
+#include <iostream>
+#include <string>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <cerrno>
+#include <cstring>
+#include <unistd.h>
+#include <strings.h>
+
+namespace Server
 {
-public:
-    udpServer()
+    using namespace std;
+    class udpServer
     {
-    }
+        enum
+        {
+            UASGE_ERR = 1,
+            SOCKET_ERR,
+            BIND_ERR
+        };
 
-    void initServer()
-    {
+    public:
+        udpServer(const u_int16_t &port, const string &ip = _defaultIp)
+            : _port(port), _ip(ip), _sockfd(-1)
+        {
+        }
 
-    }
+        void initServer()
+        {
+            // 1、创建套接字，得到一份缓冲区
+            _sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+            if (-1 == _sockfd)
+            {
+                cerr << "Socket Error: " << errno << ": " << strerror(errno) << endl;
+                exit(SOCKET_ERR);
+            }
+            // 2、绑定套接字(port,ip)
+            struct sockaddr_in local; // 定义了一个变量，用户栈
 
-    void start()
-    {
-    }
+            bzero(&local, sizeof local);
 
-    ~udpServer()
-    {
-    }
+            local.sin_family = AF_INET;
+            // port和ip需要发给对方
+            local.sin_port = htons(_port);
+            local.sin_addr.s_addr = inet_addr(_ip.c_str()); // ip:1、string->uint32_t 2、htonl()
 
-};
+            if (0 != bind(_sockfd, (struct sockaddr *)&local, sizeof local))
+            {
+                cerr << "Bind Error " << errno << ": " << strerror(errno) << endl;
+                exit(BIND_ERR);
+            }
+            
+            // UDP Server的预备工作完成
+        }
 
+        void start()
+        {
+            // 服务器的本质是一个常驻内存的进程死循环
+            // 例如：操作系统
+            for (;;)
+            {
+                sleep(1);
+            }
+        }
+
+        ~udpServer()
+        {
+        }
+
+    private:
+        inline const static string _defaultIp = "0.0.0.0"; // 默认Ip
+
+        // 标识唯一的进程
+        uint16_t _port;
+        string _ip;
+
+        int _sockfd; // socket文件描述符
+    };
+}
